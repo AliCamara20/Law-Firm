48d205d5ee3e84e185fa0a29da159e15bb440fb8
Alicamara20 <ali.camara1955@gmail.com>
2025-01-14 21:02:59 +0000

    style ctl section

--- src/CallToAction.jsx
+++ src/CallToAction.jsx
@@ -3,6 +3,10 @@ const CallToAction = () => {
    <section className="ctl_section">
     <div className="container">
         <img src={balance} alt="" className="justice_img" />
+        <p className="ctl_text">
+            Pushing your case done is what we are responsible for.
+        </p>
+        <button className="ctl_btn"><a href="" className="action_link">contact us</a></button>
     </div>
    </section>
 }
